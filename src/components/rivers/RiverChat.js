f0bb9b043305caae5e4a19a2d3574cc4a537a95b
kyliepost <kylie.postlethwaite00@gmail.com>
2021-09-16 09:58:52 -0500

    dynamic routing works and now have html rendering on page

--- src/components/rivers/RiverChat.js
+++ src/components/rivers/RiverChat.js
@@ -1,21 +1,30 @@
-import React from "react";
-import { useHistory } from "react-router-dom";
+import React, { useEffect, useState } from "react"
+import { useParams } from "react-router-dom"
 
 export const RiverChat = () => {
-    const history = useHistory()
+    const [post, setPost] = useState({}) // State variable for current ticket object
+    const [river] = useState() 
+      
+    const { postId } = useParams()  // Variable storing the route parameter
+
+    useEffect(
+        () => {
+            fetch(`http://localhost:8088/posts/${postId}/chat?_expand=riverId`)
+                .then(res => res.json())
+                .then(setPost)
+        },
+        [ postId ]  // Above function runs when the value of ticketId change
+    )
 
     return (
-        <div className="riverCard">
-            {
-                <p>
-                    <button onClick={() => history.push("/chat")}>
-                        Go to this chat forum
-                    </button>
-                </p>
-
-            }
-            
-        
-        </div>
+        <>
+        <h2>River Chat</h2>
+            <section className="post">
+                {/* <h3 className="post__description">{river.name}</h3> */}
+                <h3 className="post__description">{post.description}</h3>
+                {/* <div className="post__user">Submitted by {post.user?.name}</div> */}
+               
+            </section>
+        </>
     )
 }
