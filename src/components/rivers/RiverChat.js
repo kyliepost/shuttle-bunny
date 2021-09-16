144328757a6b2714a8250e9b91d6d786a8f52d06
kyliepost <kylie.postlethwaite00@gmail.com>
2021-09-16 14:46:42 -0500

    chat form created and routing is correct, need to work on where jsx is rendering

--- src/components/rivers/RiverChat.js
+++ src/components/rivers/RiverChat.js
@@ -5,7 +5,7 @@ export const RiverChat = () => {
     const [post, setPost] = useState({}) // State variable for current ticket object
     // const [river] = useState() 
     const history = useHistory()
-      
+
     const { riverId } = useParams()  // Variable storing the route parameter
     const { postId } = useParams()
 
@@ -17,20 +17,22 @@ export const RiverChat = () => {
                     setPost(data)
                 })
         },
-        [ riverId ]  // Above function runs when the value of ticketId change
+        [riverId]  // Above function runs when the value of ticketId change
     )
 
     return (
         <>
-        <h2>River {riverId} Chat</h2>
-        <button onClick={() => history.push(`/posts/${riverId}/create`)}>
+            <h2>River {riverId} Chat</h2>
+
+            <button onClick={() => history.push(`/posts/${riverId}/create`)}>
                 Create Post
             </button>
+
             <section className="post">
                 <h3 className="post__description">{riverId.name}</h3>
                 <h3 className="post__description">{post.description}</h3>
                 <div className="post__user">Submitted by {post.user?.name}</div>
-               
+
             </section>
         </>
     )
---
+++ src/components/rivers/RiverChatForm.js
@@ -0,0 +1,69 @@
+import React, { useState } from "react";
+import { useHistory, useParams } from "react-router";
+
+export const ChatForm = () => {
+    const [post, updatePost] = useState({
+        description: ""
+    })
+    const history = useHistory()
+
+
+    const savePost = (evt) => {
+        evt.preventDefault()
+
+        const newTicket = {
+            description: post.description,
+            userId: parseInt(localStorage.getItem("shuttle_user")),
+            riverId: 1,
+            timestamp: 1634687829221
+        }
+
+
+        const fetchOption = {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(newTicket)
+        }
+
+        return fetch("http://localhost:8088/posts", fetchOption)
+            .then(() => {
+                history.push("/posts")
+            })
+    }
+
+    return (
+        <>
+            <form>
+                <h2>Create Post</h2>
+                <fieldset>
+                    <label htmlFor="inputDescription"> Description </label>
+                    <input
+                        onChange={
+                            (evt) => {
+                                const copy = { ...post }
+                                copy.description = evt.target.value
+                                updatePost(copy)
+                            }
+                        }
+                        required autoFocus
+                        type="text"
+                        className="form-control"
+                        placeholder="description"
+                    />
+                </fieldset>
+                <fieldset>
+                    <button onClick={savePost} className="btn btn-primary">
+                        Submit Post
+                    </button>
+                </fieldset>
+                <fieldset>
+                    <button type="submit">
+                        Cancel
+                    </button>
+                </fieldset>
+            </form>
+        </>
+    )
+}
